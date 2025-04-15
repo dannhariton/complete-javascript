d8bae4df61489329cf7f0f78ad154a4b5d9bed69
Dan Hariton <dhariton@griddynamics.com>
2025-04-15 14:26:55 +0300

    add lesson exercises

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  // console.log(firstName);
+
+  function printAge() {
+    let output = `${firstName} You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1991 && birthYear <= 1996) {
+      var millennial = true;
+      const firstName = 'Steven';
+      const str = `Oh and you are a millennial ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+
+      output = 'New output';
+    }
+    // console.log(add(3, 4));
+    console.log(millennial);
+    console.log(output);
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
